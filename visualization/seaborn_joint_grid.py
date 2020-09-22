62078a6525db98e509bc7887172a956b578771da
Alexandru ILISII <ailisii@gmail.com>
2020-09-22 14:05:40 +0200

    Joint Grid examples

--- visualization/seaborn_joint_grid.py
+++ visualization/seaborn_joint_grid.py
@@ -21,6 +21,21 @@ sns.jointplot(x="hum",
         y="total_rentals",
         kind='reg',
         data=df)
+
+# Plot temp vs. total_rentals as a regression plot
+sns.jointplot(x="temp",
+         y="total_rentals",
+         kind='reg',
+         data=df,
+         order=2,
+         xlim=(0, 1))
+
+# Plot a jointplot showing the residuals
+sns.jointplot(x="temp",
+        y="total_rentals",
+        kind='resid',
+        data=df,
+        order=2)                 
       
 plt.show()
 plt.clf()
