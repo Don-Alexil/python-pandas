8f619426e9975d506e44e0d1f36900fa684ff7c2
Alexandru ILISII <ailisii@gmail.com>
2020-09-22 14:06:10 +0200

    Joint Grid examples (2)

--- visualization/seaborn_joint_grid.py
+++ visualization/seaborn_joint_grid.py
@@ -36,6 +36,23 @@ sns.jointplot(x="temp",
         kind='resid',
         data=df,
         order=2)                 
-      
+
+# Create a jointplot of temp vs. casual riders
+# Include a kdeplot over the scatter plot
+g = (sns.jointplot(x="temp",
+             y="casual",
+             kind='scatter',
+             data=df,
+             marginal_kws=dict(bins=10, rug=True))
+    .plot_joint(sns.kdeplot))
+
+# Replicate the above plot but only for registered riders
+g = (sns.jointplot(x="temp",
+             y="registered",
+             kind='scatter',
+             data=df,
+             marginal_kws=dict(bins=10, rug=True))
+    .plot_joint(sns.kdeplot))
+    
 plt.show()
 plt.clf()
